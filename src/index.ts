5f0f9ca16fa77093e0907e061845d0af62567003
Dennis Alund <dennis.alund@gmail.com>
2017-09-29 22:42:07 +0800

    Removing log

--- src/index.ts
+++ src/index.ts
@@ -112,7 +112,6 @@ export class UnifiController {
             n: quantity
         });
 
-        console.log(JSON.stringify(body));
         return this.request(`/api/s/${this._siteName}/cmd/hotspot`, body);
     }
 
